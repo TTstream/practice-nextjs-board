c4a0029752ed10d964ef5b89cf8d11195e2807cd
SeongJin Oh <guk0230@naver.com>
2023-05-05 14:01:55 +0900

    mongoDB연결 및 데이터 가져와서 보여주기

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
-export default function Home() {
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
+  // 코드 줄이는 방법
+  // const db = (await connectDB).db("forum");
+  // let result = await db.collection("post").find().toArray();
+
   return <div>ㅎㅇ</div>;
 }
